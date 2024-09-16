ddc18a18dcb6635edec62c24d550cc7d130484b3
maheshghorpade <mahesh8888647012@gmail.com>
2024-09-16 13:11:00 +0530

    Add files via upload

---
+++ oneitemtuple.py
@@ -0,0 +1,6 @@
+thistuple = ("apple",) #Create Tuple With One Item
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
---
+++ tupledatatypes.py
@@ -0,0 +1,7 @@
+tuple1 = ("apple","grapes","mango")
+tuple2 = (2,4,7)
+tuple3 = (True,False,True)
+
+print(tuple1)
+print(tuple2)
+print(tuple3)
