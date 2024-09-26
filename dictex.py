e02235b8ea12303c8ccef48b152cd7616086a6bc
maheshghorpade <mahesh8888647012@gmail.com>
2024-09-26 21:32:57 +0530

    Add files via upload

---
+++ dictex.py
@@ -0,0 +1,14 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+x = {
+    "bmw" : "sereis 7",
+    "mmotor" : "ic",
+    "year" : 2024
+}
+
+print(x)
